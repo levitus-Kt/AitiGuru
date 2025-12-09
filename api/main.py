1352458b6c6b935052a19c4e36731b498377e089
levitus-Kt <111065692+levitus-Kt@users.noreply.github.com>
2025-12-09 16:16:20 +0300

    Realize change quantity stock and fix price in order

--- api/main.py
+++ api/main.py
@@ -37,6 +37,28 @@ def add_item(order_id: int, data: AddItem):
     """, (order_id, data.product_id))
     row = cur.fetchone()
 
+    if row:
+        new_qty = row[0] + data.quantity
+        cur.execute("""
+            UPDATE order_items 
+            SET quantity=%s 
+            WHERE order_id=%s AND product_id=%s
+        """, (new_qty, order_id, data.product_id))
+    else:
+        # взять цену на момент продажи
+        cur.execute("SELECT price FROM products WHERE id=%s", (data.product_id,))
+        price = cur.fetchone()[0]
 
+        cur.execute("""
+            INSERT INTO order_items(order_id, product_id, quantity, price)
+            VALUES (%s, %s, %s, %s)
+        """, (order_id, data.product_id, data.quantity, price))
+
+    # уменьшить остатки на складе
+    cur.execute("""
+        UPDATE products 
+        SET quantity = quantity - %s 
+        WHERE id = %s
+    """, (data.quantity, data.product_id))
 
     return {"status": "ok"}
