8caad4c947d7a782f55a13b7b3f7073bd2f9bf92
levitus-Kt <111065692+levitus-Kt@users.noreply.github.com>
2025-12-11 20:09:37 +0300

    Fix typo

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://postgres:postgres@localhost:5432/mydatabase")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
